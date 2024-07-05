17cc265a22097385bbd2933ace6eeca7762661a3
designer20011121 <designer20011121@outlook.com>
2024-07-04 20:13:17 -0700

    first_transfer

---
+++ transfer.ts
@@ -0,0 +1,10 @@
+import { Transaction, SystemProgram, LAMPORTS_PER_SOL, sendAndConfirmTransaction } from "@solana/web3.js"
+const transaction = new Transaction()
+
+// const sendSolInstruction = SystemProgram.transfer({
+//   fromPubkey: sender,
+//   toPubkey: recipient,
+//   lamports: LAMPORTS_PER_SOL * amount
+// })
+
+// transaction.add(sendSolInstruction)

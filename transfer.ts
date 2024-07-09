353a76961c0c65a3e5945c9a3b3449a53b2d115e
designer20011121 <designer20011121@outlook.com>
2024-07-09 06:41:43 -0700

    first

--- transfer.ts
+++ transfer.ts
@@ -1,10 +1,89 @@
-import { Transaction, SystemProgram, LAMPORTS_PER_SOL, sendAndConfirmTransaction } from "@solana/web3.js"
-const transaction = new Transaction()
+import { getKeypairFromEnvironment, getExplorerLink } from "@solana-developers/helpers";
+import { createMint } from "@solana/spl-token";
+require("dotenv").config()
+import { Connection, clusterApiUrl,PublicKey, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
 
-// const sendSolInstruction = SystemProgram.transfer({
-//   fromPubkey: sender,
-//   toPubkey: recipient,
-//   lamports: LAMPORTS_PER_SOL * amount
-// })
+const connection = new Connection(clusterApiUrl("testnet"));
 
-// transaction.add(sendSolInstruction)
+const user = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`🔑 Loaded our keypair securely, using an env file! Our public key is: ${user.publicKey.toBase58()}`
+);
+
+const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  );
+
+// const tokenMint = await createMint(connection, user, user.publicKey, null, 2);
+
+// const link = getExplorerLink("address", tokenMint.toString(), "testnet")
+
+//console.log(`✅ Finished! Created token mint: ${link}`);
+const tokenMintAccount = new PublicKey("4xGGE77CahERcLivH2NumVBf7NVbNAZKf4gvtANWbTHM");
+
+const metadataData = {
+  name: "Solana Training Token",
+  symbol: "TRAINING",
+  // Arweave / IPFS / Pinata etc link using metaplex standard for off-chain data
+  uri: "https://arweave.net/1234",
+  sellerFeeBasisPoints: 0,
+  creators: null,
+  collection: null,
+  uses: null,
+};
+
+const metadataPDAAndBump = PublicKey.findProgramAddressSync(
+  [
+    Buffer.from("metadata"),
+    TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+    tokenMintAccount.toBuffer(),
+  ],
+  TOKEN_METADATA_PROGRAM_ID
+);
+
+const metadataPDA = metadataPDAAndBump[0];
+
+const transaction = new Transaction();
+
+const createMetadataAccountInstruction =
+  createCreateMetadataAccountV3Instruction(
+    {
+      metadata: metadataPDA,
+      mint: tokenMintAccount,
+      mintAuthority: user.publicKey,
+      payer: user.publicKey,
+      updateAuthority: user.publicKey,
+    },
+    {
+      createMetadataAccountArgsV3: {
+        collectionDetails: null,
+        data: metadataData,
+        isMutable: true,
+      },
+    }
+  );
+
+transaction.add(createMetadataAccountInstruction);
+
+const transactionSignature = await sendAndConfirmTransaction(
+  connection,
+  transaction,
+  [user]
+);
+
+const transactionLink = getExplorerLink(
+  "transaction",
+  transactionSignature,
+  "testnet"
+);
+
+console.log(`✅ Transaction confirmed, explorer link is: ${transactionLink}!`);
+
+const tokenMintLink = getExplorerLink(
+  "address",
+  tokenMintAccount.toString(),
+  "testnet"
+);
+
+console.log(`✅ Look at the token mint again: ${tokenMintLink}!`);
